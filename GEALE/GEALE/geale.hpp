ac08b412e7b8e20ee5545d3c40fc56e450b7cc8e
Máté Szabó <szmate1618@gmail.com>
2022-06-26 01:07:14 +0200

    add BinaryEncoder and BinaryDecoder

--- GEALE/GEALE/geale.hpp
+++ GEALE/GEALE/geale.hpp
@@ -1,6 +1,8 @@
 #ifndef GREATEST_EVOLUTIONARY_ALGORITHM_LIBRARY_EVER
 #define GREATEST_EVOLUTIONARY_ALGORITHM_LIBRARY_EVER
 
+#include <climits>
+
 
 namespace geale
 {
@@ -25,17 +27,30 @@ class BitArrayAdapter
 private:
 	Container container;
 	size_t bitCount;
+	size_t lastReadBitPositionPlus1;
+	size_t lastWriteBitPositionPlus1;
 
 public:
+	BitArrayAdapter();
+
 	void ReadBit(size_t bitPosition, int& value);
 	void WriteBit(size_t bitPosition, int value);
 	void AppendBit(int value);
+	void WriteOrAppendBit(size_t bitPosition, int value);
 	void FlipBit(size_t bitPosition);
+
+	size_t GetLastReadBitPositionPlus1();
+	size_t GetLastWriteBitPositionPlus1();
 };
+
+template <typename Container, typename IndexConverter>
+BitArrayAdapter<Container, IndexConverter>::BitArrayAdapter() : bitCount(0), lastReadBitPositionPlus1(0), lastWriteBitPositionPlus1(0) {}
 
 template <typename Container, typename IndexConverter>
 void BitArrayAdapter<Container, IndexConverter>::ReadBit(size_t bitPosition, int& value)
 {
+	lastReadBitPositionPlus1 = bitPosition + 1;
+
 	size_t index;
 	size_t offset;
 
@@ -47,6 +62,8 @@ void BitArrayAdapter<Container, IndexConverter>::ReadBit(size_t bitPosition, int
 template <typename Container, typename IndexConverter>
 void BitArrayAdapter<Container, IndexConverter>::WriteBit(size_t bitPosition, int value)
 {
+	lastWriteBitPositionPlus1 = bitPosition + 1;
+
 	size_t index;
 	size_t offset;
 
@@ -58,6 +75,8 @@ void BitArrayAdapter<Container, IndexConverter>::WriteBit(size_t bitPosition, in
 template <typename Container, typename IndexConverter>
 void BitArrayAdapter<Container, IndexConverter>::AppendBit(int value)
 {
+	lastWriteBitPositionPlus1 = bitCount + 1;
+
 	size_t index;
 	size_t offset;
 
@@ -74,14 +93,77 @@ void BitArrayAdapter<Container, IndexConverter>::AppendBit(int value)
 }
 
 template <typename Container, typename IndexConverter>
+void BitArrayAdapter<Container, IndexConverter>::WriteOrAppendBit(size_t bitPosition, int value)
+{
+	if (bitPosition < bitCount)
+	{
+		WriteBit(bitPosition, value);
+	}
+	else
+	{
+		AppendBit(value);
+	}
+}
+
+template <typename Container, typename IndexConverter>
 void BitArrayAdapter<Container, IndexConverter>::FlipBit(size_t bitPosition)
 {
+	lastWriteBitPositionPlus1 = bitPosition + 1;
+
 	size_t index;
 	size_t offset;
 
 	IndexConverter::GetElementIndexAndOffset(bitPosition, index, offset);
 
 	container.at(index) ^= (1 << offset);
+}
+
+template <typename Container, typename IndexConverter>
+size_t BitArrayAdapter<Container, IndexConverter>::GetLastReadBitPositionPlus1()
+{
+	return lastReadBitPositionPlus1;
+}
+
+template <typename Container, typename IndexConverter>
+size_t BitArrayAdapter<Container, IndexConverter>::GetLastWriteBitPositionPlus1()
+{
+	return lastWriteBitPositionPlus1;
+}
+
+
+template <typename T, typename BitArray>
+class BinaryEncoder
+{
+public:
+	void Encode(size_t startBitPosition, const T& value, BitArray& bitArray);
+};
+
+template <typename T, typename BitArray>
+void BinaryEncoder<T, BitArray>::Encode(size_t startBitPosition, const T& value, BitArray& bitArray)
+{
+	for (size_t i = 0; i < CHAR_BIT * sizeof(T); i++)
+	{
+		bitArray.WriteOrAppendBit(startBitPosition + i, (value & (1 << i)) >> i);
+	}
+}
+
+template <typename T, typename BitArray>
+class BinaryDecoder
+{
+public:
+	void Decode(size_t startBitPosition, T& value, const BitArray& bitArray);
+};
+
+template <typename T, typename BitArray>
+void BinaryDecoder<T, BitArray>::Decode(size_t startBitPosition, T& value, const BitArray& bitArray)
+{
+	value = 0;
+	for (size_t i = 0; i < CHAR_BIT * sizeof(T); i++)
+	{
+		int bitValue;
+		bitArray.ReadBit(startBitPosition + i, bitValue);
+		value |= (bitValue << i);
+	}
 }
 
 }
